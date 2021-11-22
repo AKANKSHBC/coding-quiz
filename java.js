01d74057697f681cff70a228c1f105fc0c2f4ca2
Akanksh BC <akankshbc@gmail.com>
2021-11-23 04:28:04 +0530

    major update

--- java.js
+++ java.js
@@ -3,12 +3,12 @@ var questions = [
     {
             "question":"1) What is the best case time complexity of deleting a node in a Singly Linked list?",
             "options":["A) O (n)", "B) O (n2)", "C) O (nlogn)", "D) O (1)"],
-            "answer":"D) O (1)"},
+            "answer":"504"},
           
     {
             "question":"2. Which of the following real world scenarios would you associate with a stack data structure?",
             "options":["a) piling up of chairs one above the other", "b) people standing in a line to be serviced at a counter", "c) offer services based on the priority of the customer", "d) tatkal Ticket Booking in IRCTC"],
-            "answer":"a) piling up of chairs one above the other"}
+            "answer":"501"}
 
     ];
 
@@ -17,7 +17,7 @@ var bvnm = document.querySelectorAll(".grid-item");
 var valueB = 0;
 
 
-function myFunction(j) {
+function questionFunction(j) {
 
         var pquestion1 = document.getElementById('pquestion1');
         pquestion1.innerHTML = questions[j].question;
@@ -31,15 +31,78 @@ function myFunction(j) {
 
         for(var i = 0; i < 4; i++) {
             buttonOptions[i].innerHTML = questions[j].options[i];
+            buttonOptions[i].id = j+100;
+
+            if(buttonOptions[i].classList.contains("btn-success")) {
+                buttonOptions[i].className = "btn btn-outline-dark col-6 ms-5 btn-lg text-start ps-5";
+            };
         };
+
+        var submitButton = document.getElementsByName("submitBut");
+    
+        for( var i=0; i<1; i++) {
+            submitButton[i].id = j+200;
+        };
+
+        console.log(submitButton);
 
 };
 
+function answerFunction(k,idn) {
+
+    
+
+    ld = idn.id;
+
+    if(k == questions[ld-100].answer ) {
+        idn.className = "btn btn-success col-6 ms-5 btn-lg text-start ps-5";
+        ld = -1;
+    } else console.log("fail" + ld);
+};
+
+function checkAnswer(n) {
+
+    ohh = n.id;
+    console.log("ohh1" + ohh);
+
+    var mainbutton = document.getElementById(ohh-200);
+    //console.log(mainbutton)
+    mainbutton.style.visibility = "hidden";
+
+    console.log("ohh2" + ohh);
+};
+
+//console.log(clm);
 
 
 
 
 //console.log(questions.q1.question);
-//console.log(buttonOptions);
+
 
 console.log(bvnm);
+
+
+
+/*
+
+var ld = -1;
+    var ks = document.getElementsByName("abc");
+    //console.log(ks);
+    
+    
+    
+    ld = idn.id;
+
+    if(k == questions[ld].answer ) {
+        idn.className = "btn btn-success col-6 ms-5 btn-lg text-start ps-5";
+        ld = -1;
+    } else console.log("fail" + ld);
+
+    if (ld == -1) {
+        for(var i=0; i<4; i++) {
+            ks[i].className = "btn btn-outline-dark col-6 ms-5 btn-lg text-start ps-5";
+        };
+    };
+
+*/
